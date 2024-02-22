7adb884ca7c2199c5022bb288f69277e90521ee3
Lê Việt Đức <71498086+levietduc0712@users.noreply.github.com>
2024-02-22 23:51:28 +0700

    first commit

---
+++ FreeRTOS_Blink_LED/Debug/blinky_task.d
@@ -0,0 +1,99 @@
+# FIXED
+
+blinky_task.obj: ../blinky_task.c
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/FreeRTOS.h
+blinky_task.obj: C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h
+blinky_task.obj: C:/Users/Admin/Documents/TivaC/FreeRTOS_Blink_LED/FreeRTOSConfig.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/projdefs.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/portable.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/deprecated_definitions.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/mpu_wrappers.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/task.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/list.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/semphr.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/queue.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_ints.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+blinky_task.obj: C:/ti/TivaWare_C_Series-2.2.0.295/examples/boards/ek-tm4c123gxl/drivers/rtos_hw_drivers.h
+
+../blinky_task.c:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdio.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdarg.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/FreeRTOS.h:
+
+C:/ti/ccs1260/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stddef.h:
+
+C:/Users/Admin/Documents/TivaC/FreeRTOS_Blink_LED/FreeRTOSConfig.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/projdefs.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/portable.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/portable/CCS/ARM_CM4F/portmacro.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/task.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/list.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/semphr.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/third_party/FreeRTOS/Source/include/queue.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_ints.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/interrupt.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/examples/boards/ek-tm4c123gxl/drivers/rtos_hw_drivers.h:
+
